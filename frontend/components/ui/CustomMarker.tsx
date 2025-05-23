83d498c7dfa6b3eec42c9d5bdd91cf3e6184d313
Khoi Nguyen <khoi.nn@kyanon.digital>
2025-05-23 17:35:22 +0700

    Refactor CustomMarker and MapScreen components to improve marker handling and state management. Update CustomMarker to accept onPress prop and utilize SharingPost type. Enhance MapScreen to manage selected markers with animations and improve marker coordinate handling.

--- frontend/components/ui/CustomMarker.tsx
+++ frontend/components/ui/CustomMarker.tsx
@@ -1,24 +1,24 @@
-/* eslint-disable react-native/no-inline-styles */
 import {Image} from 'react-native';
 import {Marker} from 'react-native-maps';
 import React from 'react';
+import {SharingPost} from '../../redux/SharingPostReducer';
 
 export const CustomMarker = ({
   marker,
+  onPress,
 }: {
-  marker: {
-    id: string | number;
-    latitude: number;
-    longitude: number;
-    title: string;
-    description?: string;
-  };
+  marker: SharingPost;
+  onPress: () => void;
 }) => {
   return (
     <Marker
-      coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+      coordinate={{
+        latitude: Number(marker.latitude),
+        longitude: Number(marker.longitude),
+      }}
       pinColor="navy"
-      title={marker.title}>
+      title={marker.title}
+      onPress={onPress}>
       <Image
         source={require('../../assets/images/map-marker.png')}
         style={{width: 24, height: (24 * 74) / 55}}
--- frontend/screens/MapScreen.tsx
+++ frontend/screens/MapScreen.tsx
@@ -1,13 +1,15 @@
 import {Checkbox, Icon, Menu} from 'react-native-paper';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 /* eslint-disable react-hooks/rules-of-hooks */
 import MapView, {LatLng, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import React, {useEffect, useMemo, useRef, useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {moderateScale, scale, verticalScale} from '../utils/scale';
 
+import {Animated} from 'react-native';
 import Colors from '../global/Color';
 import {CustomMarker} from '../components/ui/CustomMarker';
 import {RootState} from '../redux/Store';
+import {Route} from '../constants/route';
 import {SharingPost} from '../redux/SharingPostReducer';
 import {createNotifications} from 'react-native-notificated';
 import {getFontFamily} from '../utils/fonts';
@@ -18,7 +20,8 @@ const listFilter = [
   {id: 1, title: 'Trong vòng 2km', value: 2},
   {id: 2, title: 'Trong vòng 5km', value: 5},
   {id: 3, title: 'Trong vòng 10km', value: 10},
-  {id: 4, title: 'Tất cả', value: 'all'},
+  {id: 4, title: 'Trong vòng 20km', value: 20},
+  {id: 4, title: 'Tất cả', value: 100000},
 ];
 const {useNotifications} = createNotifications();
 
@@ -27,6 +30,9 @@ const MapScreen = ({navigation}: any) => {
     latitude: useSelector((state: RootState) => state.userInfo.latitude),
     longitude: useSelector((state: RootState) => state.userInfo.longitude),
   };
+  const sharingPosts = useSelector(
+    (state: RootState) => state.sharingPost.HomePage,
+  );
 
   const accessToken = useSelector((state: RootState) => state.token.key);
 
@@ -35,7 +41,11 @@ const MapScreen = ({navigation}: any) => {
 
   const [filter, setFilter] = useState<number>(100000);
   const [visible, setVisible] = useState(false);
-  const [filteredMarkers, setFilteredMarkers] = useState<SharingPost[]>([]);
+  const [filteredMarkers, setFilteredMarkers] =
+    useState<SharingPost[]>(sharingPosts);
+  const [selectedMarker, setSelectedMarker] = useState<SharingPost | null>(
+    null,
+  );
 
   const mapRef = useRef<MapView>(null);
 
@@ -51,14 +61,38 @@ const MapScreen = ({navigation}: any) => {
     }
   };
 
+  const slideAnim = useRef(new Animated.Value(200)).current; // Start below the screen
+
+  const hideInfoPanel = () => {
+    Animated.timing(slideAnim, {
+      toValue: 200,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      setSelectedMarker(null); // clear after animation completes
+    });
+  };
+
+  useEffect(() => {
+    if (selectedMarker) {
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [selectedMarker]);
+
   useEffect(() => {
     getData();
-  }, []);
+  }, [filter]);
 
   useEffect(() => {
     if (!mapRef.current) return;
+    console.log('Im here filteredMarkers', filteredMarkers.length);
 
     if (filteredMarkers.length === 0) {
+      console.log('Im here notify');
       notify('error', {
         params: {
           description: 'Không tìm thấy địa điểm nào trong khu vực này',
@@ -74,8 +108,8 @@ const MapScreen = ({navigation}: any) => {
     const coordinates: LatLng[] = [
       {latitude: location.latitude, longitude: location.longitude},
       ...filteredMarkers.map(m => ({
-        latitude: m.latitude,
-        longitude: m.longitude,
+        latitude: Number(m.latitude),
+        longitude: Number(m.longitude),
       })),
     ];
 
@@ -95,10 +129,7 @@ const MapScreen = ({navigation}: any) => {
         1000,
       );
     }
-    if (markerRef.current) {
-      markerRef.current.showCallout();
-    }
-  }, [filter, mapRef]);
+  }, [filteredMarkers, mapRef]);
 
   const handleBack = () => {
     navigation.goBack();
@@ -112,7 +143,17 @@ const MapScreen = ({navigation}: any) => {
         <TouchableOpacity onPress={handleBack}>
           <Icon source={'arrow-left'} size={25} color={Colors.white} />
         </TouchableOpacity>
-        <Text style={styles.title}>Tìm kiếm đồ ăn gần bạn</Text>
+        <Text
+          style={[
+            styles.title,
+            {
+              fontSize: moderateScale(20),
+              fontWeight: 'bold',
+              color: Colors.white,
+            },
+          ]}>
+          Tìm kiếm đồ ăn gần bạn
+        </Text>
         <Menu
           visible={visible}
           onDismiss={closeMenu}
@@ -157,24 +198,68 @@ const MapScreen = ({navigation}: any) => {
         ref={mapRef}
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
+        onPress={() => {
+          if (selectedMarker) {
+            hideInfoPanel();
+          }
+        }}
         region={{
-          latitude: 10.79943,
-          longitude: 106.68822,
+          latitude: location.latitude,
+          longitude: location.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
         <Marker
           ref={markerRef}
           coordinate={{
-            latitude: 10.79943,
-            longitude: 106.68822,
+            latitude: location.latitude,
+            longitude: location.longitude,
           }}
           title="Vị trí của bạn"
         />
         {filteredMarkers.map(marker => (
-          <CustomMarker key={marker.id} marker={marker} />
+          <CustomMarker
+            key={marker.id}
+            marker={marker}
+            onPress={() => setSelectedMarker(marker)}
+          />
         ))}
       </MapView>
+      {selectedMarker && (
+        <Animated.View
+          style={[
+            styles.filterContainer,
+            {transform: [{translateY: slideAnim}]},
+          ]}>
+          <Image
+            source={{uri: selectedMarker.images[0]}}
+            style={styles.image}
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.title}>{selectedMarker.title}</Text>
+            <Text style={styles.description}>{selectedMarker.description}</Text>
+            <View style={styles.distanceContainer}>
+              <Icon source={'map-marker'} size={24} color={Colors.greenText} />
+              <Text style={styles.distance}>
+                {selectedMarker.distance.toFixed(2)} km
+              </Text>
+            </View>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={() => {
+                navigation.navigate(Route.PostDetail2, {
+                  item: selectedMarker,
+                  location: {
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                  },
+                });
+              }}>
+              <Text style={styles.btnText}>Xem chi tiết</Text>
+            </TouchableOpacity>
+          </View>
+        </Animated.View>
+      )}
     </View>
   );
 };
@@ -185,17 +270,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: scale(10),
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
     zIndex: -1,
   },
+  distanceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: scale(4),
+  },
+  infoContainer: {
+    flex: 1,
+    padding: scale(10),
+    gap: scale(10),
+  },
+  title: {
+    fontSize: moderateScale(20),
+    fontWeight: 'bold',
+    color: Colors.black,
+    fontFamily: getFontFamily('bold'),
+  },
+  distance: {
+    fontSize: moderateScale(16),
+    color: Colors.greenText,
+    fontFamily: getFontFamily('regular'),
+  },
   filterContainer: {
     position: 'absolute',
-    bottom: 40,
-    left: 20,
-    right: 20,
+    bottom: 0,
+    left: 0,
+    right: 0,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: scale(10),
+    paddingHorizontal: scale(20),
+    gap: scale(20),
+    borderTopRightRadius: scale(10),
+    borderTopLeftRadius: scale(10),
+    backgroundColor: Colors.white,
+    zIndex: 100,
   },
   headerContainer: {
     position: 'absolute',
@@ -209,15 +327,22 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  title: {
-    fontSize: moderateScale(24),
-    fontWeight: 'bold',
-    color: Colors.white,
-    fontFamily: getFontFamily('bold'),
+  description: {
+    fontSize: moderateScale(16),
+    color: Colors.black,
+    fontFamily: getFontFamily('regular'),
   },
   floatBtn: {
     position: 'absolute',
     left: 10,
     zIndex: 100,
+  },
+  btn: {
+    borderRadius: scale(10),
+  },
+  btnText: {
+    fontSize: moderateScale(16),
+    color: '#F05C00',
+    fontFamily: getFontFamily('regular'),
   },
 });
